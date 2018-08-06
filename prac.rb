f43ae05b79424263c6899b32b8d8fa315de93a3d
Haja Trawally <hajatrawally6@gmail.com>
2018-08-06 18:22:26 +0000

    automatically backed up by learn

--- prac.rb
+++ prac.rb
@@ -2,5 +2,5 @@ first_name ="Haja"
 last_name="Trawally"
 puts "My name is "+first_name+" "+last_name
 #String Interpolation
-puts"My first name is #{first_name} and my last name is #{last_name}""
+puts"My first name is #{first_name} and my last name is #{age.to_s}""
 age = 13
